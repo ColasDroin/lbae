7e2794bb04e468f6e39da54cc08be42110d5c18b
ColasDroin <colasdroin@gmail.com>
2022-01-13 18:12:17 +0100

    First notebook with 3D transparency data

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-# ! check this
-# TO CORRECT: https://dev.to/codemouse92/dead-simple-python-project-structure-and-imports-38c6
+# Preliminary imports
 import logging
 import os
 from lbae.modules.tools.misc import logmem
@@ -30,7 +29,6 @@ from lbae import index
 
 server = index.app.server
 
-
 # Run the app locally
 if __name__ == "__main__":
     logging.info("Starting app" + logmem())
@@ -40,23 +38,26 @@ if __name__ == "__main__":
         # Shut reddis server
         os.system("../redis/redis-6.2.6/src/redis-cli shutdown")
 
-
-# To run the app from the server, use the following command in the base lbae folder:
-# gunicorn main:server -b:8050 --workers=4
-# pkill -P1 gunicorn
-
 # TODO State of the art 3D representation of lipid expression
 # TODO Make lipid expression comparison between 3D structure
 # TODO make a careful memory analysis to free more memory
 # TODO incoporate new brain slices
-# TODO adress all #! comments in the code
+# TODO adress all # ! and # ? comments in the code
 # TODO when everything is stable, create a function that delete all pickle files and repickle everything automatically
+# TODO update notebooks with up-to-date classes
 # TODO do quality control, especially when several users are on the app in parallel
 # TODO write missing docstrings
 # TODO write documentation
+# TODO Redesign repo according to : https://dev.to/codemouse92/dead-simple-python-project-structure-and-imports-38c6
 # TODO make a docker container for the very final version of the app
 
 # ? implement the app as a multipage app when the feature is available on Dash
 # ? Implement a "fast mode" with lower resolution images and spectra to have a faster app (for slow connections)
 # ? make layout perfect for every screen size. Maybe automate the process for a given figure shape?
 # ? have the documentation always open on the right on very big screens to fill empty space
+
+
+# To run the app from the server, use the following command in the base lbae folder:
+# gunicorn main:server -b:8050 --workers=4
+# To kill gunicorn if it doesn't want to die
+# pkill -P1 gunicorn
