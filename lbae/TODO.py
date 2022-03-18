810d42f008b5fb8135f90e8eb1896e1cd51359cf
ColasDroin <colasdroin@gmail.com>
2022-03-18 14:37:46 +0100

    Updated TODO list

--- lbae/TODO.py
+++ lbae/TODO.py
@@ -1,24 +1,26 @@
 #
-# TODO make a careful memory analysis to free more memory
-# TODO replace all "tabs" by "pages"
-# TODO incoporate new brain slices
-# TODO adress all # ! and # ? comments in the code
+# TODO Finish redesign
+# TODO Do a round of debug
+# TODO Make a careful memory analysis to free more memory
+# TODO Replace all "tabs" by "pages"
+# TODO Incoporate new brain slices
+# TODO Adress all # ! and # ? comments in the code
 # TODO Find out what must be precomputed and do a function to precompute it
-# TODO make the app with tabs instead of pages to accelerate it?
-# TODO move layouts to other files
-# TODO when everything is stable, create a function that delete all pickle files and repickle everything automatically
-# TODO update notebooks with up-to-date classes
-# TODO do quality control, especially when several users are on the app in parallel
-# TODO correct the name of the files to download
-# TODO write missing docstrings
-# TODO write documentation
+# TODO Make the app with tabs instead of pages to accelerate it?
+# TODO Move layouts to other files
+# TODO When everything is stable, create a function that delete all pickle files and repickle everything automatically
+# TODO Update notebooks with up-to-date classes
+# TODO Do quality control, especially when several users are on the app in parallel
+# TODO Correct the name of the files to download
+# TODO Write missing docstrings
+# TODO Write documentation
 # TODO Add an icon for documentation in the left bar
 # TODO Make a tour with dash_tour_component?
 # TODO Redesign repo according to : https://dev.to/codemouse92/dead-simple-python-project-structure-and-imports-38c6
-# TODO make a docker container for the very final version of the app
+# TODO Make a docker container for the very final version of the app
 
-# ? implement the app as a multipage app when the feature is available on Dash
-# ? make layout perfect for every screen size. Maybe automate the process for a given figure shape?
+# ? Implement the app as a multipage app when the feature is available on Dash
+# ? Make layout perfect for every screen size. Maybe automate the process for a given figure shape?
 
 # http://cajal.epfl.ch:8050/
 
