29cb04a8f8d0ed9f65ff7b984e44e44377b9c93a
ColasDroin <colasdroin@gmail.com>
2022-08-08 11:22:47 +0200

    Fixed layout and long callbacks

--- TODO.py
+++ TODO.py
@@ -1,11 +1,7 @@
 ##### Current TODOs #####
 # TODO Do quality control and debug, especially when several users are on the app in parallel
-# TODO Update README when scRNAseq page is up to date
-# TODO Fix long callbacks
-# TODO Big lipid selection 3D when changing brain
 # TODO Solve bug masks lower left part of screen
-# TODO Implement new features from Dash 1.4 and 1.5 and 1.6
-# TODO Correlate cell type atlas with lipid atlas 
+# TODO Correlate cell type atlas with lipid atlas
 
 ##### Finalization process #####
 # TODO Check that all force_update are set to False
@@ -14,7 +10,3 @@
 # TODO Make a docker container for the very final version of the app
 # TODO Clean all notebooks output before last commit
 # TODO Delete TODO file
-
-##### Possibilities #####
-# ? Split and comment the layouts?
-# ? Build basic technical documentation for the app?
