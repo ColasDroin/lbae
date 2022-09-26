42be34777491c4204e340cbb91ba48ce4a282e10
ColasDroin <colasdroin@gmail.com>
2022-09-26 13:40:39 +0200

    Cleaned all notebooks

--- TODO.py
+++ TODO.py
@@ -3,9 +3,6 @@
 # TODO Replace the elastic net regression with a "feature importance selection" from a random forest regression
 
 ##### Finalization process #####
-# TODO Check that all force_update are set to False
-# TODO Update links in the documentation, the technical documentation, the github readme
-# TODO Check that readme and documentations are correct and up to date
 # TODO Adress remaining # ! and # ? comments in the code
 # TODO Make a docker container for the very final version of the app
 # TODO Clean all notebooks output before last commit
